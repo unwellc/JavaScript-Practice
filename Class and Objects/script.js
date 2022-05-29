d42f981b8d6cb9d0a530d1ef81376caed5ca99b9
unwellc <106143619+unwellc@users.noreply.github.com>
2022-05-29 21:46:22 +0800

    Update script.js
    backup

--- Class and Objects/script.js
+++ Class and Objects/script.js
@@ -1,11 +1,6 @@
 /**
- * Practice: Building objects
- *
- * - Create JavaScript objects based on objects in your current environment.
- * - Give each object an identifiable name.
- * - Create properties to describe the objects and set their values.
- * - Find an object that has another object inside of it to create a nested object.
- * - Test your objects in the browser console by accessing the entire object and its specific properties.
+ * Practice Script
+ * Created by Anwille Catindig
  */
 
 //Create and Define Vape and Phone object based from Vape and Phone class
